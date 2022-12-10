6ac70abd2db74cdb473574d4ab7132d5af24fd7c
Angelinakwakye <angelinakwakye03@gmail.com>
2022-12-10 06:00:43 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point for the program.
+ *
+ * Return: Always 0 (success).
+ **/
+int  main(void)
+{
+	print f("with proper grammar, but the outcome is a piece of are,\n");
+	return (0);
+
+}
+
