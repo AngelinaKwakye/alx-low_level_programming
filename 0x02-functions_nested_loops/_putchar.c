f1cdc417492faac4feba743c0693db9668d45821
Angelinakwakye <angelinakwakye03@gmail.com>
2022-12-13 08:58:23 -0800

    pushfile

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* _putchar - alternates for original putcahr() function from stand
+* @x: Character parameter
+*
+*Return: void.
+*/
+
+void _putchar(char x)
+{
+	putchar(x);
+}
+
