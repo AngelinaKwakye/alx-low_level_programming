cd0e11ed15a7e6c1f2d4ffd387294d707514261b
AngelinaKwakye <angelinakwakye03@gmail.com>
2022-12-23 08:28:25 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,10 @@
 #include <unistd.h>
 
 /**
- ** main - Entry point
- *
+ * main - Entry point
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
  * Return: Always 1 (Required)
- **/
-
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
