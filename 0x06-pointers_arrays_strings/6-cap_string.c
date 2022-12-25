ea0c38d8fdad0f2c2a125921bce1b7bffc9d1d19
AngelinaKwakye <Angelinakwakye>
2022-12-25 15:38:05 +0000

    cap srings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,27 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i, j;
 
-	while (str[i])
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-				i == 0)
-			str[i] -= 32;
-		i++;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
-	return (str);
+	return (s);
 }
+
