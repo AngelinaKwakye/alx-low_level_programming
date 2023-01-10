e0fd3edc3ed303a6a40db63833e6f551ff1aa4e9
AngelinaKwakye <Angelinakwakye>
2023-01-10 12:24:54 +0000

    array of chars

--- 0x08-recursion/0x0B-malloc_free/0-create_array.c
+++ 0x08-recursion/0x0B-malloc_free/0-create_array.c
@@ -1,20 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * create_array - create array of size and assign char c
- * @size: size of array
- * @c: char to assign
- * Return: pointer to array, NULL if fail
+ *create_array - creates an array of char
+ *@size: size of array
+ *@c: char integer
+ *Return: pointer to array or NULL if fail
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-	unsigned int i;
-		str = (malloc(sizeof(char) * size));
-		if (size == 0 || str == NULL)
-		return (NULL);
-		for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+char *p = malloc(size);
+if (size == 0 || p == 0)
+return (0);
+while (size--)
+p[size] = c;
+return (p);
 }
